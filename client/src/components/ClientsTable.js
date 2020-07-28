b9d5b97e72a608e42a1efdb0146550b58ca431d4
Ivan Alexis Carbajal Sandin <icarbajals0600@gmail.com>
2020-07-28 01:03:45 -0500

    Buscar y Añadir clientes

---
+++ client/src/components/ClientsTable.js
@@ -0,0 +1,113 @@
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
+import TableRow from '@material-ui/core/TableRow';
+// import { fetchData } from '../utils';
+
+const columns = [
+    { id: 'nombre', label: 'Name', minWidth: 100 },
+    { id: 'rfc', label: 'RFC', minWidth: 100 },
+    { id: 'email', label: 'Email', minWidth: 100 },
+    { id: 'telefono', label: 'Teléfono', minWidth: 100 },
+    {
+        id: 'credito',
+        label: 'Credito',
+        minWidth: 100,
+        align: 'right',
+        format: (value) => value.toFixed(2).toLocaleString('en-US'),
+    },
+    {
+        id: 'deudaActual',
+        label: 'Deuda',
+        minWidth: 100,
+        align: 'right',
+        format: (value) => value.toFixed(2).toLocaleString('en-US'),
+    }
+];
+
+const useStyles = makeStyles({
+    root: {
+        width: '100%',
+    },
+    container: {
+        maxHeight: 440,
+    },
+});
+
+export default function ClientsTable({ data }) {
+    const classes = useStyles();
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+    const [rows, setRows] = useState([]);
+
+    useEffect(() => {
+        // async function getData() {
+        //     const data = await fetchData('/api/clients', 'GET');
+        //     setRows(data);
+        // }
+        // getData();
+        setRows(data);
+    }, [data]);
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
+    return (
+        <Paper className={classes.root}>
+            <TableContainer className={classes.container}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {columns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    {column.label}
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            return (
+                                <TableRow hover role="checkbox" tabIndex={-1} key={row.rfc}>
+                                    {columns.map((column) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell key={column.id} align={column.align}>
+                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5]}
+                component="div"
+                count={rows.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onChangePage={handleChangePage}
+                onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+        </Paper>
+    );
+}
